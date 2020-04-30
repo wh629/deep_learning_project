89844e4e5f394de7f820bf0954e3fe412c672dcf
William Huang <60375376+wh629@users.noreply.github.com>
2020-04-30 17:37:21 -0400

    Update model.py
    Temporary

--- src/model.py
+++ src/model.py
@@ -4,22 +4,33 @@ Module to define our model architecture
 
 import torch
 import torch.nn as nn
+import torchvision
 import os
 
 class Model(nn.Module):
-    def __init__(self,
-                 #TO DO
-                 ):
+    def __init__(self):
         
         """
         TO DO
         """
-        
         super(Model, self).__init__()
+        self.image_w = image_w
+        self.road_w = road_w        
         
+        self.image_detect = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=False, progress=True, num_classes=9, pretrained_backbone=False)
+        self.transform = self.image_detect.transform
+        self.backbone = self.image_detect.backbone
+        
+        #TODO get into right output shape
+        self.road = nn.Sequential(
+            nn.Conv2d(...)
+            nn.ReLU()
+            ...
+        )
 
     def forward(self,
-                #TO DO
+                images = None,
+                box_targets = None,
                 ):
         """
         Forward
@@ -27,6 +38,12 @@ class Model(nn.Module):
         Returns output as specified below
 
         """
-        outputs = None
+        # do image detection
+        out_box = self.image_detect(images, box_targets)
+        
+        # do road map
+        images, targets = self.transform(images, None)
+        features = self.backbone(images.tensors)
+        out_road = self.road(features)
     
-        return outputs  # (loss)...
+        return out_box, out_road
