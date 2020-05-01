42492d31480dccb220a1aef77e9bf24f9df86446
wh629 <60375376+wh629@users.noreply.github.com>
2020-04-30 21:57:20 -0400

    updates

--- src/learner.py
+++ src/learner.py
@@ -20,25 +20,25 @@ import helper
 
 class Learner():
     def __init__(self,
-                 access_mode,
-                 experiment_name,
-                 model,
-                 device,
-                 myio,
-                 save_dir,
-                 max_steps = 1e5,
-                 log_int = 1e4,
-                 best_int = 500,
-                 verbose_int = 1000,
-                 max_grad_norm = 1.0,
-                 optimizer = None,
-                 weight_decay = 0.0,
-                 lr = 5e-3,
-                 eps = 1e-8,
-                 accumulate_int = 1,
-                 batch_size = 8,
-                 warmup_pct = 0.0,
-                 save = False,
+                 access_mode      = None,   # os access mode for created files
+                 experiment_name  = None,   # name of experiment
+                 model            = None,   # model
+                 device           = None,   # device to run experiment
+                 myio             = None,   # myio.IO object for loading data
+                 save_dir         = None,   # directory to save results
+                 max_steps        = 1e5,    # maximum number of update steps
+                 best_int         = 500,    # interval for checking weights
+                 verbose_int      = 100,    # interval for logging information
+                 max_grad_norm    = 0.0,    # maximum gradients to avoid exploding gradients
+                 optimizer        = None,   # optimizer for training
+                 weight_decay     = 0.0,    # weight decay if using
+                 lr               = 0.01,   # learning rate
+                 eps              = 1e-8,   # epsilon to use for adam
+                 accumulate_int   = 1,      # number of steps to accumulate gradients before stepping
+                 batch_size       = 8,      # batch size
+                 warmup_pct       = 0.0,    # percent of updates used to warm-up learning rate
+                 save             = False,  # whether to save weights
+                 patience         = 5,      # number of checks with no improvement before early stop
                  ):
         """
         Object to store learning. Used for fine-tuning.
@@ -52,7 +52,6 @@ class Learner():
         self.IO = myio
         self.save_dir = save_dir
         self.max_steps = max_steps
-        self.log_int = log_int
         self.best_int = best_int
         self.verbose_int = verbose_int
         self.max_grad_norm = max_grad_norm
@@ -63,6 +62,7 @@ class Learner():
         self.batch_size = batch_size
         self.warmup_pct = warmup_pct
         self.save = save
+        self.patience = patience
         
         # make directory for recorded weights if doesn't already exist
         self.log_dir = os.path.join(self.save_dir, 'logged')
@@ -100,37 +100,43 @@ class Learner():
         return optimizer
     
     def pack_input(self,
-                   ):
+                   batch = None):
         """
-        TO DO:
-        
-        pack input into dictionary
+        Labeled Data Batch: image, target, road_image, extra
+        Unlabeled Data Batch: image, camera_index
+        --------------------
+        Returns:
+        Dictionary of batch information with keys as model keywords
+
         """
         
-        inputs = {}
+        if self.labeled:
+            inputs = {"images"       : batch[0],
+                      "box_targets"  : batch[1],
+                      "road_targets" : batch[2]}
+        else:
+            inputs = {"images"       : batch[0]}
         
         return inputs
         
     
     def train_step(self,
-                   batch=None,
-                   idx=None,
-                   scheduler=None,
-                   optimizer=None,
-                   accumulated=None
+                   batch        = None,    # batch data
+                   idx          = None,    # index of step
+                   scheduler    = None,    # learning rate scheduler
+                   optimizer    = None,    # optimizer for training
+                   accumulated  = None,    # number of accumulated gradients so far
                    ):
         """
         Training for a single batch.
         
         --------------------
         Returns:
-        
+        l           - loss of the batch
+        accumulated - accumulated 
         """
-# =============================================================================
-#         # TO DO: 
-#         # unpack data and labels from batch and send to device
-# =============================================================================
-        inputs = self.pack_input() # TO IMPLEMENT
+        self.model.train()
+        inputs = self.pack_input(batch)
         
         # zero gradients
         if accumulated == 0:
@@ -153,7 +159,8 @@ class Learner():
         
         # clip gradients
         if accumulated == self.accumulate_int:
-            nn.utils.clip_grad_norm_(self.model.parameters(), self.max_grad_norm)
+            if self.max_grad_norm > 0:
+                nn.utils.clip_grad_norm_(self.model.parameters(), self.max_grad_norm)
             
             #take a step in gradient descent
             optimizer.step()
@@ -166,27 +173,16 @@ class Learner():
         
         return l.detach(), accumulated
         
-    def evaluate(self,
-                 model = None
-                 ):
+    def evaluate(self):
         """
         Evaluation model on task.
         
         ---------------------
         Returns:
         Average validation loss
-        """
-        # assign model if None
-        if model is None:
-            model = self.model
-        else:
-            if torch.cuda.is_available() and torch.cuda.device_count() > 1 and not isinstance(model, nn.DataParallel):
-                # multiple GPUs
-                model = torch.nn.DataParallel(model)
-            model.to(self.device)
-            
+        """            
         # puts model in evaluation mode
-        model.eval()
+        self.model.eval()
         
         cum_loss = 0
         
@@ -203,16 +199,16 @@ class Learner():
 #                 # TO DO:
 #                 # unpack data and labels from batch and send to device
 # =============================================================================
-                inputs = self.pack_input() # TO IMPLEMENT
+                inputs = self.pack_input(batch) # TO IMPLEMENT
                 
-                out = model(**inputs)
+                out = self.model(**inputs)
                 
                 l = out[0]
                 
                 if isinstance(self.model, nn.DataParallel):
                     l = l.mean()
                 
-                cum_loss += l.detach()
+                cum_loss += l
         
         #TODO: calculate ts road map and ats bounding boxes
         # might need to loop for road_ts
@@ -225,8 +221,9 @@ class Learner():
         return cum_loss/(i+1), road_ts, box_ats
         
     def train(self,
-              optimizer = None,
-              scheduler = None,
+              optimizer = None,  # optimizer to use for training
+              scheduler = None,  # scheduler to use for training
+              labeled = True,    # whether training on labeled data
               ):
         """
         Fine-tune model on task
@@ -243,35 +240,37 @@ class Learner():
         
         if scheduler is None:
             scheduler = opt.lr_scheduler.OneCycleLR(optimizer,
-                                                    max_lr = self.lr,
-                                                    tota_steps=self.max_steps,
-                                                    anneal_strategy="linear",
-                                                    cycle_momentum=False,
-                                                    pct_start=self.warmup_pct)
+                                                    max_lr          = self.lr,
+                                                    tota_steps      = self.max_steps,
+                                                    anneal_strategy = "linear",
+                                                    cycle_momentum  = False,
+                                                    pct_start       = self.warmup_pct
+                                                    )
         
+        self.labeled = labeled
+
         cum_loss =  0.0
         best_val_loss = float("inf")
         best_val_road = 0.0
         best_val_image = 0.0
         best_iter = 0
         exp_log_dir = os.path.join(self.log_dir, self.experiment_name)
-        
-        # make directory for model weights for given task if doesn't exist
-        if not os.path.exists(exp_log_dir):
-            os.mkdir(exp_log_dir)
+        stop = False
+        no_improve = 0
+
+        if self.save:
+            # make directory for model weights for given task if doesn't exist
+            if not os.path.exists(exp_log_dir):
+                os.mkdir(exp_log_dir)
         
         best_path = os.path.join(exp_log_dir, 'best.pt')
         
-# =============================================================================
-#         # TO DO: load data
-# =============================================================================
-        self.train_dataloader = self.IO.load_dataloader(None)
-        self.val_dataloader = self.IO.load_dataloader(None)
+        self.train_dataloader, self.val_dataloader = self.IO.load_dataloader(labeled=self.labeled)
         
         # set number of epochs based on number of iterations
         max_epochs = (self.max_steps // (len(self.train_dataloader.dataset)//(self.batch_size*self.accumulate_int))) + 1
         
-        log.info("Training with {} iterations ~ {} epochs".format(self.max_steps, max_epochs))
+        log.info("Training with {} iterations; ~ {} epochs".format(self.max_steps, max_epochs))
 
         # train
         global_step = 0
@@ -280,23 +279,20 @@ class Learner():
         train_iterator = trange(0, int(max_epochs), desc = 'Epoch', mininterval=30)
         start = time.time()
         
-        optimizer.zero_grad()
+        self.model.zero_grad()
         for epoch in train_iterator:
-            epoch_iterator = tqdm(self.train_dataloader, desc='Epoch Iteration', mininterval=30)
+            epoch_iterator = tqdm(self.train_dataloader, desc='Train Iteration', mininterval=30)
             for step, batch in enumerate(epoch_iterator):
-                
-                self.model.train()
-                iter_loss, accumulated = self.train_step(batch=batch,
-                                                         idx=global_step,
-                                                         scheduler=scheduler,
-                                                         optimizer=optimizer,
-                                                         accumulated=accumulated)
+                iter_loss, accumulated = self.train_step(batch       = batch,
+                                                         idx         = global_step,
+                                                         scheduler   = scheduler,
+                                                         optimizer   = optimizer,
+                                                         accumulated = accumulated)
+                cum_loss += iter_loss
 
                 if accumulated == 0:
                     global_step += 1
 
-                cum_loss += iter_loss
-                
                 # check for best every best_int
                 if global_step % self.best_int == 0:
                     log.info("="*40+" Evaluating on step: {}".format(global_step))
@@ -312,16 +308,6 @@ class Learner():
                     if val_results < best_val_loss:
                         best_val_loss = val_results
                         best_iter = global_step
-                        
-                        if self.save:
-                            # for multi-gpu
-                            if isinstance(self.model, nn.DataParallel):
-                                best_state_dict = self.model.module.state_dict()
-                            else:
-                                best_state_dict = self.model.state_dict()
-                            
-                            torch.save(best_state_dict, best_path)
-                            os.chmod(best_path, self.access_mode)
                     
                     # check for best road ts
                     if val_road > best_val_road:
@@ -331,36 +317,60 @@ class Learner():
                     if val_image > best_val_image:
                         best_val_image = val_image
 
+                    # save if either best evaluation metric saved
+                    if val_road > best_val_road or val_image > best_val_image:
+                        if self.save:
+                            # for multi-gpu
+                            if isinstance(self.model, nn.DataParallel):
+                                best_state_dict = self.model.module.state_dict()
+                            else:
+                                best_state_dict = self.model.state_dict()
+                            
+                            torch.save(best_state_dict, best_path)
+                            os.chmod(best_path, self.access_mode)
+                    else:
+                        no_improve += 1
+                        if no_improve >= self.patience:
+                            log.info('='*40+' Early stopping at step {} '.format(global_step)+'='*40)
+                            stop = True
+
+
                 # write to log every verbose_int
                 if global_step % self.verbose_int == 0:
                     log.info('='*40+' Iteration {} of {} | Average Training Loss {:.6f} |'\
-                             ' Best Val F1 {} | Best Iteration {} |'.format(
+                             ' Best Val Road ts {} | Best Val Box ats {} | Best Iteration {} |'.format(
                                  global_step,
                                  self.max_steps,
                                  cum_loss/global_step,
-                                 best_val_loss,
+                                 best_val_road,
+                                 best_val_image,
                                  best_iter
                         )
                     )
                 
-                global_step += 1
-                
                 # break training if max steps reached (+1 to get max_step)
-                if global_step > self.max_steps+1:
+                if global_step > self.max_steps or stop:
                     epoch_iterator.close()
                     break
-            if global_step > self.max_steps+1:
+            if global_step > self.max_steps or stop:
                 train_iterator.close()
                 break
             
         # log finished results
         log.info('Finished | Average Training Loss {:.6f} |'\
-                 ' Best Val F1 {} | Best Iteration {} | Time Completed {:.2f}s'.format(
+                 ' Best Val Road ts {} | Best Val Box ats {} | Best Iteration {} | Time Completed {:.2f}s'.format(
                      cum_loss/global_step,
-                     best_val_loss,
+                     best_val_road,
+                     best_val_image,
                      best_iter,
                      time.time()-start
                 )
             )
         
-        return {"best_val_loss" : best_val_loss, "best_val_road" : best_val_road, "best_val_image" : best_val_image, "best_val_step" : best_iter, "best_weights" : best_path}
+        return {"best_val_loss" : best_val_loss,
+                "best_val_road" : best_val_road,
+                "best_val_image" : best_val_image,
+                "best_val_step" : best_iter,
+                "best_weights" : best_path,
+                "current_step" : global_step,
+                "max_step" : self.max_steps}
