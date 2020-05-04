5c0bc32e53c62095331f0d471562062be4f1c2e1
wh629 <60375376+wh629@users.noreply.github.com>
2020-05-04 10:47:23 -0400

    included convolution in init

--- src/pretraining/pretrain.py
+++ src/pretraining/pretrain.py
@@ -114,6 +114,7 @@ class CameraEncoder(nn.Module):
                                                                                  pretrained_backbone=False)
 
         self.resnet = self.image_detect.backbone
+        self.conv_256_1 = nn.Conv2d(in_channels=256, out_channels=1, kernel_size=3, stride=1, padding=1)
         self.double_dim_minus1 = nn.ConvTranspose2d(in_channels=256, out_channels=256, kernel_size=3, stride=2,
                                                     padding=1)
         self.relu = nn.ReLU()
