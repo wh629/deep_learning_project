774887efc18704536a49a18bc4a4efea0ec62af0
wh629 <60375376+wh629@users.noreply.github.com>
2020-05-06 17:01:04 -0400

    fixed pretraining

--- src/pretraining/pretrain.py
+++ src/pretraining/pretrain.py
@@ -196,36 +196,44 @@ def generate_random_image_mask(channels, height, width):
 
     return mask
 
-def eval(loader, model, permutations, permutations_k, device, idx, debug):
+def evaluate(val_batches, val_answers, model, device, idx, debug):
     log.info(f"Evaluating at step {idx}")
     model.eval()
     correct = 0
     with torch.no_grad():
-        for i, batch_old in enumerate(tqdm(loader, desc='Eval', mininterval=30)):
-            batch = repackage_image_batch(batch_old)
-
-            batch_size_now = batch.shape[0]
-            # generate answers
-            answers = torch.randint(permutations_k, (batch_size_now,)).to(device)
-
-            # prepare input
-            for ith in range(batch_size_now):
-                batch[ith] = batch[ith, permutations[answers[ith].item()]]
-                for jth in range(6):
-                    random_mask = generate_random_image_mask(*batch[0, 0].shape)
-                    batch[ith, jth] *= random_mask
-
-            batch = batch.to(device)
+        for i, batch, answer in enumerate(tqdm(zip(val_batches, val_answers), desc='Eval', mininterval=30)):
+            batch, answer = batch.to(device), answer.to(device)
             output, pred = model(batch)
 
-            correct += torch.eq(pred.float(), answers.float()).sum().item()
+            correct += torch.eq(pred.float(), answer.float()).sum().item()
 
             if i == 1 and debug:
                 log.info("Debug break evaluation")
                 break
 
     model.train()
-    return correct/len(loader.dataset) # returns the accuracy
+    return correct/len(val_batches) # returns the accuracy
+
+def get_val_perms(loader, permutations, permutations_k):
+    batches = []
+    answers = []
+
+    with torch.no_grad():
+        for i, batch_old in enumerate(tqdm(loader, desc='Eval', mininterval=30)):
+            batch = repackage_image_batch(batch_old)
+
+            batch_size_now = batch.shape[0]
+            # generate answers
+            answer = torch.randint(permutations_k, (batch_size_now,))
+
+            # prepare input
+            for ith in range(batch_size_now):
+                batch[ith] = batch[ith, permutations[answer[ith].item()]]
+
+            batches.append(batch)
+            answers.append(answer)
+
+    return batches, answers
 
 def pretrain(parser, batch_size=5, permutations_k=64):
     max_grad_bound = 1
@@ -273,6 +281,8 @@ def pretrain(parser, batch_size=5, permutations_k=64):
                                                 batch_size=batch_size,
                                                 shuffle=False,
                                                 num_workers=2)
+
+    val_batches, val_answers = get_val_perms(pre_train_val, permutations, permutations_k)
 
     model = CameraEncoder(permutations_k).to(device)
 
@@ -335,7 +345,7 @@ def pretrain(parser, batch_size=5, permutations_k=64):
 
             if global_step % parser.save_steps == 0 and not checked:
                 checked = True
-                current_acc = eval(pre_train_val, model, permutations, permutations_k, device, global_step, parser.debug)
+                current_acc = evaluate(val_batches, val_answers, model, device, global_step, parser.debug)
                 log.info(f"Current Acc {current_acc} | Current Step {global_step} | Previous Best {best_acc} | Best Step {best_step}")
 
                 if current_acc >= best_acc:
